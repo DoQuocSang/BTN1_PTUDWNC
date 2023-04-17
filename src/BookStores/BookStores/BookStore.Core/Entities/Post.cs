379db4a819c2d6fc7dad78b8c19f7ac878e639dc
quangvansuong <quangvansuong.2014485@gmail.com>
2023-04-17 14:57:29 +0700

    bắt đầu tạo Database

---
+++ src/BookStores/BookStores/BookStore.Core/Entities/Post.cs
@@ -0,0 +1,40 @@
+﻿using BookStore.Core.Contracts;
+
+namespace BookStore.Core.Entities;
+
+public class Post : IEntity
+{
+    public int Id { get; set; }
+    // Tiêu đề bài viết
+    public string Title { get; set; }
+    // Mô tả ngắn về nội dung
+    public string ShortDescription { get; set; }
+    // Nội dung chi tiết của bài viết
+    public string Description { get; set; }
+    public string Meta { get; set; }
+    public string UrlSlug { get; set; }
+    public string ImageUrl { get; set; }
+    //// Số Lượt xem
+    //public int ViewCount { get; set; }
+    //// Trạng thái bài viét
+    //public bool Published { get; set; }
+
+    // Ngày giờ đăng
+    public DateTime PostedDate { get; set; }
+    // Ngày cập nhật lần cuối
+    public DateTime? ModifiedDate { get; set; }
+    // Mã chuyên mục
+    public int CategoryId { get; set; }
+
+    // Mã tác giả bài viết
+    public int AuthorId { get; set; }
+
+    // Chuyên mục bài viết
+    public Category Category { get; set; }
+    // Tác giả bài viết
+    public Author Author { get; set; }
+
+    // Danh sách từ khóa bài viết
+    public IList<Tag> Tags { get; set; }
+
+}
---
+++ src/BookStores/BookStores/BookStore.Data/Contexts/BookStoreDbContext.cs
@@ -0,0 +1,34 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using BookStore.Core.Entities;
+using BookStore.Data.Mappings;
+
+
+namespace BookStore.Data.Contexts;
+
+public class BookStoreDbContext : DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // Chú ý thay chuổi kết nối cho phù hợp
+        //optionsBuilder.UseSqlServer(@"Server =LAPTOP-GEIT9Q0O;Database=BookStore;
+        //                Trusted_Connection=True;MultipleActiveResultSets=true");
+        optionsBuilder.UseSqlServer(@"Server=LAPTOP-GEIT9Q0O;TrustServerCertificate=True;Database=BookStore;
+                Trusted_Connection=True;MultipleActiveResultSets=true");
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(CategoryMap).Assembly);
+    }
+}
+// còn lệnh "Add-Migration InitialCreate" Phần 4 Trang 11 
