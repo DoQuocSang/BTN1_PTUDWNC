5a66103df2b91ede4bece6e395c8606667a36e50
quangvansuong <quangvansuong.2014485@gmail.com>
2023-05-04 18:41:56 +0700

    thêm mới dữ liệu

--- src/web-api/TatBlog.Data/Seeders/DataSeeder.cs
+++ src/web-api/TatBlog.Data/Seeders/DataSeeder.cs
@@ -190,6 +190,7 @@ namespace TatBlog.Data.Seeders
                     }
                 },
 
+
                 new()
                 {   
                     Title = "Mắt biếc",
@@ -379,24 +380,7 @@ namespace TatBlog.Data.Seeders
                     }
                 },
 
-                new()
-                {
-                    Title = "Section 1.10.33 of \"de Finibus Bonorum et Malorum\"",
-                    ShortDescription = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores ",
-                    Description = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.",
-                    Meta = "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores ",
-                    UrlSlug = "section-11033-of-\"de-finibus-bonorum-et-malorum\"",
-                    Published = true,
-                    PostedDate = new DateTime(2022, 9, 11, 11, 0, 0),
-                    ModifiedDate = null,
-                    ViewCount = 45,
-                    Author = authors[3],
-                    Category = categories[2],
-                    Tags = new List<Tag>()
-                    {
-                        tags[3]
-                    }
-                },
+               
 
                 new()
                 {
